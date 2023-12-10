6b8e9a094d353ab47a765bc3f040f2c7452d6f07
Endymion Kasanardjo <zetafish@gmail.com>
2023-12-10 11:06:43 +0100

    day 3 - part 2

--- 2023/src/d3.clj
+++ 2023/src/d3.clj
@@ -33,21 +33,39 @@
                   (list [y (dec x1)] [y x2])
                   (map #(vector (inc y) %) (range (dec x1) (inc x2))))))
 
-(defn sum-of-the-parts [engine]
+(defn find-parts [engine]
   (->> engine
        (map-indexed find-digits)
        (apply concat)
        (map (fn [{:keys [y x1 x2] :as m}]
-              (let [border (around engine y x1 x2)]
-                (-> m
-                    (assoc :border border
-                           :symbols (->> border
-                                         (map #(get-in engine %))
-                                         (filter engine-symbol?)
-                                         seq))))))
-       (filter :symbols)
-       (map :n)
+              (let [border (around engine y x1 x2)
+                    values (map #(get-in engine %) border)]
+                (assoc m :symbols
+                       (->> (mapcat (fn [b v] (when (engine-symbol? v)
+                                                [b v]))
+                                    border values)
+                            (filter some?)
+                            seq)))))
+       (filter :symbols)))
+
+(defn sum-of-parts [parts]
+  (reduce + (map :n parts)))
+
+(defn sum-of-gears [parts]
+  (->> parts
+       (map #(select-keys % [:n :symbols]))
+       (filter #(= \* (second (:symbols %))))
+       (sort-by (comp vec flatten :symbols))
+       (partition-by :symbols)
+       (filter #(= 2 (count %)))
+       (map #(reduce * (map :n %)))
        (reduce +)))
 
-(sum-of-the-parts (read-engine "d3_small.txt"))
-(sum-of-the-parts (read-engine "d3.txt"))
+(def example (read-engine "d3_small.txt"))
+(def data (read-engine "d3.txt"))
+
+(sum-of-parts (find-parts example))
+(sum-of-parts (find-parts data))
+
+(sum-of-gears (find-parts example))
+(sum-of-gears (find-parts data))
