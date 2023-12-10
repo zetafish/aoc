9dd76d59be5c022530db7ca717ec33fe7845b69a
Endymion Kasanardjo <zetafish@gmail.com>
2023-12-10 13:20:11 +0100

    Day 4 - part 2

--- 2023/src/d4.clj
+++ 2023/src/d4.clj
@@ -3,6 +3,9 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
+(defn parse-int [s]
+  (Integer/parseInt s))
+
 (defn pow [x n]
   (reduce * (repeat n x)))
 
@@ -11,18 +14,36 @@
         id (parse-int (second (re-matches #"Card +(\d+)" card)))
         [winners haves] (str/split numbers #" +\| +")
         winners (map parse-int (str/split winners #" +"))
-        haves (map parse-int (str/split haves #" +"))]
-    {:id id
-     :winners (set winners)
-     :haves (set haves)
-     :score (let [n (count (filter (set winners) haves))]
-              (if (zero? n)
-                0
-                (pow 2 (dec n))))}))
+        haves (map parse-int (str/split haves #" +"))
+        matches (count (filter (set winners) haves))]
+    [id {:matches matches
+         :score (if (zero? matches) 0 (pow 2 (dec matches)))}]))
 
 (defn read-cards [f]
-  (map parse-card (str/split-lines (slurp (io/resource f)))))
+  (into {} (map parse-card (str/split-lines (slurp (io/resource f))))))
 
-(reduce + (map :score (read-cards "d4_small.txt")))
-(reduce + (map :score (read-cards "d4.txt")))
+(defn play-round [cards {:keys [todo pile]}]
+  (if (empty? todo)
+    {:pile pile}
+    {:pile (+ pile (reduce + (map second todo)))
+     :todo (->> todo
+                (mapcat (fn [[id n]]
+                          (->> (range (:matches (cards id)))
+                               (map #(+ 1 id %))
+                               (map #(vector % n)))))
+                (sort-by first)
+                (partition-by first)
+                (map (fn [coll] [(ffirst coll) (reduce + (map second coll))])))}))
+
+(defn scratch-till-you-drop [cards]
+  (let [state {:pile 0 :todo (frequencies (keys cards))}]
+    (->> (iterate (partial play-round cards) state)
+         (drop-while (comp seq :todo))
+         first)))
+
+(reduce + (map :score (vals (read-cards "d4_small.txt"))))
+(reduce + (map :score (vals (read-cards "d4.txt"))))
+
+(scratch-till-you-drop (read-cards "d4_small.txt"))
+(scratch-till-you-drop (read-cards "d4.txt"))
 
