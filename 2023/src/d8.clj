fb990b59a60733f665f603c9c876324c15191afc
Endymion Kasanardjo <zetafish@gmail.com>
2023-12-14 00:10:01 +0100

    Day 8 - part 2

--- 2023/src/d8.clj
+++ 2023/src/d8.clj
@@ -1,28 +1,51 @@
 (ns d8
   "Haunted Wasteland"
   (:require [clojure.java.io :as io]
+            [clojure.math.numeric-tower :refer [lcm]]
             [clojure.string :as str]))
 
-(defn parse-code [s]
-  (map {\L :left \R :right} s))
+(defn parse-dirs [s]
+  (mapv {\L 0 \R 1} s))
 
-(defn parse-node [s]
+(defn parse-nodes [s]
   (let [[_ a b c] (re-matches #"(\w+) = \((\w+), (\w+)\)" s)]
-    [a {:left b :right c}]))
+    [a [b c]]))
 
 (defn read-network [f]
-  (let [coll (str/split-lines (slurp (io/resource f)))]
-    {:code (parse-code (first coll))
-     :nodes (into {} (map parse-node (drop 2 coll)))}))
+  (let [coll (str/split-lines (slurp (io/resource f)))
+        graph (map parse-nodes (drop 2 coll))
+        labels (sort (distinct (concat (map first graph)
+                                       (mapcat second graph))))
+        label->num (zipmap labels (iterate inc 0))
+        num->label (zipmap (iterate inc 0) labels)]
+    {:dirs (parse-dirs (first coll))
+     :labels labels
+     :label->num label->num
+     :num->label num->label
+     :graph (->> graph
+                 (sort-by first)
+                 (map second)
+                 (mapv (partial mapv label->num)))}))
 
-(defn step [state dir]
-  (assoc state :loc (get-in (:nodes state) [(:loc state) dir])))
+(defn select-nodes [network suffix]
+  (->> (:labels network)
+       (filter #(str/ends-with? % suffix))
+       (map (:label->num network))))
 
-(defn count-the-steps [network from to]
-  (->> (reductions step
-                   (assoc network :loc from)
-                   (cycle (:code network)))
-       (take-while #(not= to (:loc %)))
-       count))
+(defn count-the-steps [{:keys [graph dirs] :as network} from to]
+  (let [next-node (fn [node dir] (get-in graph [node dir]))
+        dir (fn [counter] (get dirs (mod counter (count dirs))))]
+    (loop [counter 0 from from]
+      (cond
+        (contains? to from) counter
+        :else (recur (inc counter)
+                     (next-node from (dir counter)))))))
 
-(count-the-steps (read-network "d8.txt") "AAA" "ZZZ")
+(defn solve [network from-suffix to-suffix]
+  (let [from (select-nodes network from-suffix)
+        to (set (select-nodes network to-suffix))
+        steps (map #(count-the-steps network % to) from)]
+    (reduce lcm (first steps) (rest steps))))
+
+(println (solve (read-network "d8.txt") "AAA" "ZZZ"))
+(println (solve (read-network "d8.txt") "A" "Z"))
